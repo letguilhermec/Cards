27c98d91bf6734a956f57d036175273c65f1554f
Guilherme Correa <correacgui@gmail.com>
2023-05-01 16:36:26 -0300

    feat: creates view file for single card

---
+++ SingleCardView.swift
@@ -0,0 +1,20 @@
+//
+//  SingleCardView.swift
+//  Cards
+//
+//  Created by Guilherme de Carvalho Correa on 01/05/23.
+//
+
+import SwiftUI
+
+struct SingleCardView: View {
+  var body: some View {
+    Color.yellow
+  }
+}
+
+struct SingleCardView_Previews: PreviewProvider {
+  static var previews: some View {
+    SingleCardView()
+  }
+}

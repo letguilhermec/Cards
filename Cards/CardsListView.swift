205ecbc6c7de5c31f39d0fe6e8a310495078e998
Guilherme Correa <correacgui@gmail.com>
2023-05-01 16:39:57 -0300

    feat: creates a full screen modal view

--- Cards/CardsListView.swift
+++ Cards/CardsListView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct CardsListView: View {
+  @State private var isPresented = false
+  
   var body: some View {
     ScrollView(showsIndicators: false) {
       VStack {
         ForEach(0..<10) { _ in
           CardThumbnail()
+            .onTapGesture {
+              isPresented = true
+            }
         }
       }
+    }
+    .fullScreenCover(isPresented: $isPresented) {
+      SingleCardView()
     }
   }
 }

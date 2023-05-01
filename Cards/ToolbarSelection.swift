602d4bba99c33a5e7477d2c0b04338e88609622b
Guilherme Correa <correacgui@gmail.com>
2023-05-01 17:37:42 -0300

    feat: creates enum for the 4 toolbar btns

---
+++ Cards/ToolbarSelection.swift
@@ -0,0 +1,12 @@
+//
+//  ToolbarSelection.swift
+//  Cards
+//
+//  Created by Guilherme de Carvalho Correa on 01/05/23.
+//
+
+import Foundation
+
+enum ToolbarSelection {
+  case photoModal, frameModal, stickerModal, textModal
+}

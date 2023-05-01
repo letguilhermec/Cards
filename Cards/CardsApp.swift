080c6b813cd94ca110e7f566cd078b598f9cdb3e
Guilherme Correa <correacgui@gmail.com>
2023-05-01 16:15:44 -0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Guilherme de Carvalho Correa on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

954b5622a3a0c0b6d1a4afc88b47ddd5a7efd0fc
Guilherme Correa <correacgui@gmail.com>
2023-05-02 11:24:42 -0300

    feat: StickerModal renders all stickers in folder

--- Cards/Views/Single Card Views/Card Modal Views/StickerModal.swift
+++ Cards/Views/Single Card Views/Card Modal Views/StickerModal.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct StickerModal: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var stickerNames: [String] = []
+  
+  static func loadStickers() -> [String] {
+    var themes: [URL] = []
+    var stickerNames: [String] = []
+    
+    let fileManager = FileManager.default
+    if let resourcePath = Bundle.main.resourcePath,
+       let enumerator = fileManager.enumerator(
+        at: URL(fileURLWithPath: resourcePath + "/Stickers"),
+       includingPropertiesForKeys: nil,
+       options: [
+        .skipsSubdirectoryDescendants,
+        .skipsHiddenFiles
+       ]) {
+      for case let url as URL in enumerator
+      where url.hasDirectoryPath {
+        themes.append(url)
+      }
     }
+    
+    for theme in themes {
+      if let files = try?
+          fileManager.contentsOfDirectory(atPath: theme.path) {
+        for file in files {
+          stickerNames.append(theme.path + "/" + file)
+        }
+      }
+    }
+    
+    return stickerNames
+  }
+  
+  func image(from path: String) -> UIImage {
+    print("loading:", path)
+    return UIImage(named: path)
+    ?? UIImage(named: "error-image")
+    ?? UIImage()
+  }
+  
+  var body: some View {
+    ScrollView {
+      ForEach(stickerNames, id: \.self) { sticker in
+        Image(uiImage: image(from: sticker))
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+      }
+    }
+    .onAppear {
+      stickerNames = Self.loadStickers()
+    }
+  }
 }
 
 struct StickerModal_Previews: PreviewProvider {
-    static var previews: some View {
-        StickerModal()
-    }
+  static var previews: some View {
+    StickerModal()
+  }
 }

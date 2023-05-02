76a58782c05197722f44cd2c5dd3943a5c256d52
Guilherme Correa <correacgui@gmail.com>
2023-05-02 14:02:49 -0300

    feat: now possible to add photos from library

--- Cards/Views/Single Card Views/Card Modal Views/PhotosModal.swift
+++ Cards/Views/Single Card Views/Card Modal Views/PhotosModal.swift
@@ -18,6 +18,24 @@ struct PhotosModal: View {
       matching: .images) {
         ToolbarButton(modal: .photoModal)
       }
+      .onChange(of: selectedItems) { items in
+        for item in items {
+          item.loadTransferable(type: Data.self) { result in
+            Task {
+              switch result {
+              case .success(let data):
+                if let data,
+                   let uiImage = UIImage(data: data) {
+                  card.addElement(uiImage: uiImage)
+                }
+              case .failure(let failure):
+                fatalError("Image transfer failed: \(failure)")
+              }
+            }
+          }
+        }
+        selectedItems = []
+      }
   }
 }
 

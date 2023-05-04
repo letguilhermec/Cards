eb74169221e3ef1b7d0e57a9784b595c87b52ca5
Guilherme Correa <correacgui@gmail.com>
2023-05-04 14:32:46 -0300

    feat: toolbar btns for regular and compact views

--- Cards/Views/Single Card Views/BottomToolbar.swift
+++ Cards/Views/Single Card Views/BottomToolbar.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ToolbarButton: View {
+  @Environment(\.verticalSizeClass) var verticalSizeClass
   let modal: ToolbarSelection
   
   private let modalButton: [
@@ -22,13 +23,34 @@ struct ToolbarButton: View {
   var body: some View {
     if let text = modalButton[modal]?.text,
        let imageName = modalButton[modal]?.imageName {
-      VStack {
-        Image(systemName: imageName)
-          .font(.largeTitle)
-        Text(text)
+      if verticalSizeClass == .compact {
+        compactView(imageName)
+      } else {
+        regularView(imageName, text)
       }
-      .padding(.top)
     }
+  }
+  
+  func regularView(
+    _ image: String,
+    _ text: String
+  ) -> some View {
+    VStack(spacing: 2) {
+      Image(systemName: image)
+      Text(text)
+    }
+    .frame(minWidth: 60)
+    .padding(.top, 5)
+  }
+  
+  func compactView(
+    _ image: String
+  ) -> some View {
+    VStack(spacing: 2) {
+      Image(systemName: image)
+    }
+    .frame(minWidth: 60)
+    .padding(.top, 5)
   }
 }
 
